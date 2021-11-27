dc1a344715f3f94df5e94d2adf724cf7d62cf4cf
Felipe <86751220+felipemelendez@users.noreply.github.com>
2021-11-27 09:04:17 -0800

    Header file for my_printf

---
+++ hfile.h
@@ -0,0 +1,9 @@
+#ifndef HFILE_H
+#define HFILE_H
+
+int my_printf(const char* format, ...); 
+char* converter(unsigned int number, int base); 
+char* my_strcpy(char* param_1, char* param_2);
+size_t my_strlen(char *param_1);
+
+#endif
